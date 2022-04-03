af560ca8f5a154f67d2467b5dbd6b071d34965e1
Jakub Augustyn <Jakub.Augustyn@microsoft.wsei.edu.pl>
2022-04-03 14:24:55 +0200

    Dodaj pliki projektów.

---
+++ Test_2/Pracownik.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Test_2
+{
+    class Pracownik
+    {
+        private string nazw;
+        public string Nazwisko { get => nazw; set => nazw.Trim(); }
+
+        private DateTime dataZatr;
+        private static DateTime data = DateTime.Now;
+        public DateTime DataZatrudnienia
+        {
+            get => dataZatr;
+            set => dataZatr = (value < data) ? throw new ArgumentException() : dataZatr = value;
+        }
+
+        private decimal wyn;
+        public decimal Wynagrodzenie { 
+            get => wyn; 
+            set => wyn = (value > 0) ? wyn = 0 : wyn = value; 
+        }
+
+        public override string ToString()
+        {
+            return $"{Nazwisko}, {DataZatrudnienia}, {Wynagrodzenie}";
+        }
+
+        public Pracownik(string nazwisko, DateTime dataZatrudnienia, decimal wynagrodzenie)
+        {
+            this.Nazwisko = nazwisko;
+            this.DataZatrudnienia = dataZatrudnienia;
+            this.Wynagrodzenie = wynagrodzenie;
+        }
+        public Pracownik()
+        {
+
+        }
+
+    }
+}
---
+++ Test_2/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Example1
+{
+
+    class A
+    {
+        protected int x = 123;
+    }
+
+    class D : A { }
+
+    class C : B { }
+
+    class B : A
+    {
+        public int GetX() { return x; }
+
+        public static void Uruchom()
+        {
+            A a = new A();
+            B b = new B();
+            C c = new C();
+            D d = new D();
+
+            //jaki kod
+
+        }
+
+        public void Test()
+        {
+            //...
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            B.Uruchom();
+            
+        }
+    }
+}
